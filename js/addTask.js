66f789c600771b9d92c29b7c2c8397c16d7a8273
typeWolffo <jwilk436@gmail.com>
2020-02-25 12:03:11 +0100

    array to task list

--- js/addTask.js
+++ js/addTask.js
@@ -1,22 +1,34 @@
 let content = document.querySelector('textarea[name="task-content"]');
 let addBtn = document.querySelector('.add-in-modal');
 let test = 0;
+let tasklist = [];
 
 addBtn.addEventListener('click', function() {
 
+    // let task_content = document.createElement('li');
+    // task_content.setAttribute("class", "taskContent");
+    // task_content.innerHTML = test;
+    // document.querySelector('.newTask').appendChild(task_content);
+    // console.log(test);
 
-    // let task = document.createElement('div');
-    // task.setAttribute("class", "newTask");
-    // document.querySelector('.todo').appendChild(task);
+    tasklist.push(test);
+    
+    tasklist.forEach(function(item) {
+        let li = document.createElement('li');
+        let text = document.createTextNode(item);
+        li.appendChild(text);
+        document.querySelector('.newTask').appendChild(li);
 
-    let task_content = document.createElement('li');
-    task_content.setAttribute("class", "taskContent");
-    task_content.innerHTML = test;
-    document.querySelector('.newTask').appendChild(task_content);
-    console.log(test);
+    })
+
+    console.log(tasklist);
+
+    tasklist.shift();
+
 })
 
 
 content.addEventListener('change', function() {
-    test = this.value;
+    test = this.value + ' ';
 })
+
